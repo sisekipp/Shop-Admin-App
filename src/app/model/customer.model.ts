0464356570772f4e80922bbae2bdfc9a2ec7592e
Sebastian Kipping <kipping.sebastian@gmail.com>
2017-05-25 00:08:01 +0200

    Add more customer information

--- src/app/model/customer.model.ts
+++ src/app/model/customer.model.ts
@@ -1,13 +1,25 @@
 
+import {Order} from "./order.model";
 export class Customer {
 
   private _firstName: string;
   private _lastName: string;
+  private _street: string;
+  private _city: string;
+  private _postcode: string;
+  private _birthdate: string;
+  private _orders: Order[];
 
-  constructor(firstName: string, lastName: string) {
+  constructor(firstName: string, lastName: string, birthdate: string, orders: Order[] ,street?: string, city?: string, postcode?: string) {
     this._firstName = firstName;
     this._lastName = lastName;
+    this._birthdate = birthdate;
+    this._street = street;
+    this._city = city;
+    this._postcode = postcode;
+    this._orders = orders;
   }
+
 
   get firstName(): string  {
     return this._firstName;
@@ -17,4 +29,31 @@ export class Customer {
     return this._lastName;
   }
 
+  get birthdate(): string {
+    return this._birthdate;
+  }
+
+  get street(): string {
+    return this._street;
+  }
+
+  get city(): string {
+    return this._city;
+  }
+
+  get postcode(): string {
+    return this._postcode;
+  }
+
+  get orders(): Order[] {
+    return this._orders;
+  }
+
+  get orderCount(): number {
+    if(!this._orders) {
+      return 0;
+    }
+
+    return this._orders.length;
+  }
 }
--- src/app/services/customer/customer.service.ts
+++ src/app/services/customer/customer.service.ts
@@ -5,18 +5,18 @@ export class CustomerService {
 
   public getCustomers(): Customer[] {
     return [
-      new Customer('Sebastian', 'Kipping'),
-      new Customer('Katrin', 'Kipping'),
-      new Customer('Phillipp', 'Kipping'),
-      new Customer('Heike', 'Kipping'),
-      new Customer('Jörg', 'Kipping'),
-      new Customer('Rolf', 'Kipping'),
-      new Customer('Margot', 'Kipping'),
-      new Customer('Tanja', 'Traumwieser'),
-      new Customer('Andi', 'Vogt'),
-      new Customer('Thomas', 'Traumwieser'),
-      new Customer('Carina', 'Jehle'),
-      new Customer('Till', 'Jehle')
+      new Customer('Sebastian', 'Kipping','08.12.1983',null,'Am Bach 10','Kandern','79400'),
+      new Customer('Katrin', 'Kipping','29.03.2982',null,'Am Bach 10','Kandern','79400'),
+      new Customer('Phillipp', 'Kipping','20.07.2017',null,'Am Bach 10','Kandern','79400'),
+      new Customer('Heike', 'Kipping','',null),
+      new Customer('Jörg', 'Kipping','',null),
+      new Customer('Rolf', 'Kipping','',null),
+      new Customer('Margot', 'Kipping','',null),
+      new Customer('Tanja', 'Traumwieser','',null),
+      new Customer('Andi', 'Vogt','',null),
+      new Customer('Thomas', 'Traumwieser','',null),
+      new Customer('Carina', 'Jehle','',null),
+      new Customer('Till', 'Jehle','',null)
     ]
   }
 
